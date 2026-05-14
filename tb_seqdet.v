5f3613af9ef235894e59b7273247ba296567cbe0
Bahar <berlyna05@gmail.com>
2026-05-14 18:30:48 +0330

    Initial testbench setup

---
+++ tb_seqdet.v
@@ -0,0 +1,19 @@
+module tb_seqdet ();
+    reg a, clk = 0, reset; wire y; reg[31:0] aa = 32'b00010011011001010010110001001001
+    secdet det1 ( .a(a), .clk(clk), .reset(reset), .y(y))
+
+    always #10 clk = ~clk;
+
+    initial begin
+        reset = 1;
+        a = 0;
+        @posedge(clk):
+        #1 reset = 0;
+        repeat (32) begin
+            a = aa[31];
+            aa = aa << 1;
+            @posedge(clk);
+            #1
+        end
+    end
+endmodule
