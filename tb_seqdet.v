7c4d3566e75f105fa82422f06de9df5683f9a9dd
Bahar <berlyna05@gmail.com>
2026-05-14 18:32:23 +0330

    Fix module wiring + cleanup

--- tb_seqdet.v
+++ tb_seqdet.v
@@ -1,22 +1,22 @@
 module tb_seqdet ();
     reg a, clk = 0, reset;
     wire y;
-    reg[31:0] aa = 32'b00010011011001010010110001001001;
-    
-    secdet det1 ( .a(a), .clk(clk), .reset(reset), .y(y));
+    reg[31:0] aa = 32'b10010010100100101001001010010010;
+
+    seqdet det1 ( .a(a), .clk(clk), .reset(reset), .y(y));
 
     always #10 clk = ~clk;
 
     initial begin
         reset = 1;
         a = 0;
-        @posedge(clk);
+        @(posedge clk);
         #1 reset = 0;
         repeat (32) begin
             a = aa[31];
             aa = aa << 1;
-            @posedge(clk);
-            #1
+            @(posedge clk);
+            #1;
         end
     end
 endmodule
